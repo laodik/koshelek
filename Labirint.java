8f68bfc5eae7d403c8da3b15bc55f3506663a225
Алексей Горбань <44604665+laodik@users.noreply.github.com>
2019-03-06 10:14:32 +0300

    Добавлен вывод в файл и время выполнения.

--- Labirint.java
+++ Labirint.java
@@ -3,14 +3,16 @@ import java.io.FileReader;
 import java.util.Scanner;
 import java.util.ArrayList;
 
+
 class Main
 {
     private static Integer currentIndex=2;
     private static String endPathStatus= "In progress";
     private static Integer arrayLineSize;
     private static Integer arrayColumnSize;
+    private static Long startTime;
 
-    static  ArrayList<String> getMap() throws FileNotFoundException{
+    static  ArrayList<String> getMap() throws IOException {
         ArrayList<String> myMap = new ArrayList<String>();
         FileReader mapFile = new FileReader( "map.txt" );
         Scanner mapScanner = new Scanner(mapFile);
@@ -18,6 +20,7 @@ class Main
             myMap.add(mapScanner.nextLine());
         }
         mapScanner.close();
+        mapFile.close();
         return (myMap);
     }
 
@@ -62,7 +65,57 @@ class Main
         return (Map);
     }
 
-    public static void main(String [] args) throws FileNotFoundException {
+    static void resultsToFile (String map [][]) throws IOException {
+        Integer stepNumber=1;
+        int x=0;
+        int finishX=-1;
+        int finishY=-1;
+        while (x < arrayLineSize) {
+            for (int y = 0; y < arrayColumnSize; y++) {
+                if (map[x][y].equals(Integer.toString(stepNumber))) {
+                    if (stepNumber==1) map[x][y] = "S";
+                    else map[x][y] = "X";
+                    finishX=x;
+                    finishY=y;
+                    stepNumber++;
+                    x=-1;
+                    y=0;
+                    break;
+                }
+            }
+            x++;
+        }
+        if (finishX!=-1) map[finishX][finishY] = "F";
+        //Cleaning the map field.
+        for (x = 0; x < arrayLineSize;x++) {
+            for (int y = 0; y < arrayColumnSize; y++) {
+                if ((map[x][y]!="-1")&&(map[x][y]!="X")&&(map[x][y]!="S")&&(map[x][y]!="F")) {
+                    map[x][y]="0";
+                }
+            }
+        }
+        recordingFile (map);
+    }
+
+    static void recordingFile (String map [][]) throws IOException {
+        FileWriter resultMapFile = new FileWriter("mapResult.txt");
+        for (int x = 0; x < arrayLineSize;x++) {
+            for (int y = 0; y < arrayColumnSize; y++)
+                resultMapFile.write(map[x][y] + " ");
+            resultMapFile.write("\r\n");
+        }
+        resultMapFile.write("\r\n");
+        resultMapFile.write("Минимальное число шагов - "+(currentIndex-1));
+        Long stopTime = System.currentTimeMillis();
+        Long duration = stopTime-startTime;
+        resultMapFile.write("\r\n");
+        resultMapFile.write("Время выполнения программы - "+duration+"мс");
+        resultMapFile.close();
+    }
+
+
+    public static void main(String [] args) throws IOException {
+        startTime = System.currentTimeMillis();
         ArrayList<String> myMap = getMap();
         arrayLineSize = myMap.size();
         arrayColumnSize = myMap.get(0).toString().length()/2;
@@ -78,5 +131,6 @@ class Main
             System.out.println("");
         }
         System.out.println("Минимальное число шагов - "+(currentIndex-1));
+        resultsToFile(resultPath);
     }
 }
